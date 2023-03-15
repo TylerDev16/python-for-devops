9a4d5e3e19aea7fc848f47ed6ab799957c045ef1
TylerDev16 <tylert3210@outlook.com>
2023-03-15 12:15:29 +0000

    adding command line tool

--- devopslib/randomfruit.py
+++ devopslib/randomfruit.py
@@ -6,5 +6,15 @@ def fruit():
     return choices(fruits)[0]
 
 
+def meal(beverage):
+  my_fruit = fruit()
+  print(f"Your fruit is {my_fruit} and your beverage is {beverage} ")
+  if my_fruit == "apple":
+      complete_meal= f"Your meal is a {my_fruit} and {beverage}"
+      return complete_meal
+  alternate_meal = f"Your meal is a steak and {beverage}"
+  return alternate_meal
+
+
 # var = 1
 # var = var
