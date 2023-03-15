bb33a466ad4acf5bcf75e087e5afdcfd22b5909f
TylerDev16 <tylert3210@outlook.com>
2023-03-15 12:23:50 +0000

    fixed bad indent

--- devopslib/randomfruit.py
+++ devopslib/randomfruit.py
@@ -7,13 +7,13 @@ def fruit():
 
 
 def meal(beverage):
-  my_fruit = fruit()
-  print(f"Your fruit is {my_fruit} and your beverage is {beverage} ")
-  if my_fruit == "apple":
-      complete_meal= f"Your meal is a {my_fruit} and {beverage}"
-      return complete_meal
-  alternate_meal = f"Your meal is a steak and {beverage}"
-  return alternate_meal
+    my_fruit = fruit()
+    print(f"Your fruit is {my_fruit} and your beverage is {beverage} ")
+    if my_fruit == "apple":
+        complete_meal = f"Your meal is a {my_fruit} and {beverage}"
+        return complete_meal
+    alternate_meal = f"Your meal is a steak and {beverage}"
+    return alternate_meal
 
 
 # var = 1
--- test_devopslib.py
+++ test_devopslib.py
@@ -4,7 +4,8 @@ from devopslib.randomfruit import fruit, meal
 def test_fruit():
     fruit_choice = fruit()
     assert fruit_choice in ["apple", "banana", "cherry", "pineapple"]
-    
+
+
 def test_meal():
     result = meal("milk")
     assert "milk" in result
